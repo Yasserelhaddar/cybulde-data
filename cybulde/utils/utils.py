f3f483571bc5124182b2be52361ca47b6d28affc
Yasserelhaddar <yasserelhaddar@gmail.com>
2024-01-07 20:56:37 +0100

    Updated version of the data from v0 to v1

--- cybulde/utils/utils.py
+++ cybulde/utils/utils.py
@@ -10,8 +10,10 @@ def get_logger(name: str) ->  logging.Logger:
 def run_shell_command(cmd: str) -> str:
     try:
         return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True).stdout
-    except Exception as e:
-        print(e)
-        return subprocess.run(cmd, text=True, shell=True, check=True, capture_output=True).stdout
+    except:
+        try:
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
-        
+                
